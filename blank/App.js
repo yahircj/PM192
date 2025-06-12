5a5cf29163b9d58f0ffab4c01cbd3d7ae8ee5d5a
yahircj <130490177+yahircj@users.noreply.github.com>
2025-06-11 18:19:02 -0600

    crear componente
    texto con contenido dinamico

--- blank/App.js
+++ blank/App.js
@@ -1,15 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+/*Zona de importaciones
+*/
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
+const Texto=(props)=>{
+  const {contenido}=props
+  return(
+        <Text>{contenido}</Text>
+  )
+}
+
+/*Zona 2 de ejecución 
+  MAIN
+*/
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Texto contenido= "Hola"> </Texto>
+      <Texto contenido= "mundo"> </Texto>
+      <Texto contenido= "Peregrino"> </Texto>
+      <Button title= "i´m a button"></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+/*Zona 3 de estilos */
 const styles = StyleSheet.create({
   container: {
     flex: 1,

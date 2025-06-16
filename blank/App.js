78981b46e07e115a91131d7aa9709cefa0743c16
yahircj <130490177+yahircj@users.noreply.github.com>
2025-06-16 15:00:01 -0600

    Práctica 6

--- blank/App.js
+++ blank/App.js
@@ -6,11 +6,11 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import React, { useState } from 'react';
 
 
-const Texto=()=>{
+const Texto=({style})=>{
   const [contenido, setContenido]=useState('hola pokemitas')
   const actualizarTexto=()=>{setContenido('Estado actualizado')}
   return(
-        <Text onPress={actualizarTexto}>{contenido}</Text>
+        <Text style={[styles.text, style]} onPress={actualizarTexto}>{contenido}</Text>
   )
 }
 
@@ -20,9 +20,9 @@ const Texto=()=>{
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto > </Texto>
-      <Texto > </Texto>
-      <Texto > </Texto>
+      <Texto style={styles.red}> </Texto>
+      <Texto style={styles.black}> </Texto>
+      <Texto style={styles.pink}> </Texto>
       <Button title= "i´m a button"></Button>
       <StatusBar style="auto" />
     </View>
@@ -34,7 +34,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'base-line',
     justifyContent: 'center',
+    flexDirection: 'column',
   },
+  text:{
+    color:'yellow',
+    fontSize:40,
+    height: 100,
+  },
+  red:{ backgroundColor:'orange'},
+  black:{ backgroundColor:'pink'},
+  pink:{backgroundColor:'black'},
 });
